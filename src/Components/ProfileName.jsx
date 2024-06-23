f0532a2697007ef64bc0a7d7c0dd9a92bd34fa1b
Sahil Chopade <chopade.sahil02@gmail.com>
2024-06-23 14:14:18 +0530

    changed name color

--- src/Components/ProfileName.jsx
+++ src/Components/ProfileName.jsx
@@ -5,7 +5,7 @@ const ProfileName = () => {
     <div className="flex flex-col righteous h-2/3">
       <h3 className="text-white text-[3rem]">Hi, I'm</h3>
       <div>
-        <h2 className="text-red-800 text-[8rem] font-extrabold">Sahil Chopade</h2>
+        <h2 className="text-[8rem] font-extrabold bg-gradient-to-r from-[#03a9f4] to-[#ff0058] bg-clip-text text-transparent">Sahil Chopade</h2>
       </div>
     </div>
   );
